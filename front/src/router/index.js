f75183fde9f11f7f9e480e2f8b1a8d4a5532f3fe
JoannaTabbi <joanna.tabbi@gmail.com>
2022-07-05 11:52:11 +0200

    complete routes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,13 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "@/views/Home";
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from "@/views/Home"
+import Signup from "@/views/Signup"
 import Login from "@/views/Login"
+import Notifications from "@/views/Notifications"
+import Profile from "@/views/Profile"
+import Search from "@/views/Search"
+import UserProfile from "@/views/UserProfile"
+import Trends from "@/views/Trends"
+ 
 
 const routes = [
     {
@@ -9,10 +16,41 @@ const routes = [
         component: Home
     },
     {
+        path: '/signup', 
+        name: "Signup",
+        component: Signup
+    },
+    {
         path: '/login', 
         name: "Login",
         component: Login
+    },
+    {
+        path: '/notifications', 
+        name: "Notificatons",
+        component: Notifications
+    },
+    {
+        path: '/profile', 
+        name: "Profile",
+        component: Profile
+    },
+    {
+        path: '/search', 
+        name: "Search",
+        component: Search
+    },
+    {
+        path: '/userProfile', 
+        name: "UserProfile",
+        component: UserProfile
+    },
+    {
+        path: '/trends', 
+        name: "Trends",
+        component: Trends
     }
+
 ]
 
 const router = createRouter({
@@ -21,4 +59,4 @@ const router = createRouter({
     routes, // short for `routes: routes`
   });
 
-export default router;
+export default router
