573b398610197353d46b3fc73af1dcb713b2fefe
JoannaTabbi <joanna.tabbi@gmail.com>
2022-10-14 16:30:02 +0200

    add post services

--- front/src/_services/post.services.js
+++ front/src/_services/post.services.js
@@ -1,16 +1,48 @@
 import Axios from '@/_interceptors/axios'
 
-//upload post image service: 
-
-function createPost(formData) {
-
-    return Axios.post("/posts/", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data"
-      }
+//upload post image and message: 
+let createPost = (formData) => {
+    return Axios.post("/posts", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
     })
-  }
+};
+
+// gets all the posts published
+let getPosts = () => {
+    return Axios.get("/posts")
+};
+
+//gets ont post
+let getOnePost = (id) => {
+    return Axios.get("/posts/" + id)
+}
+
+//updates one post
+let updatePost = (id, formData) => {
+    return Axios.put("/posts/" + id, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+}
+
+// likes one post
+let likePost = (id) => {
+    return Axios.post("/posts/" + id + "/like")
+}
+
+//deletes one post 
+let deletePost = (id) => {
+    return Axios.delete("/posts/" + id)
+}
 
 export const postServices = {
-    createPost
-  }
+    createPost,
+    getPosts,
+    getOnePost,
+    updatePost,
+    likePost,
+    deletePost
+}
